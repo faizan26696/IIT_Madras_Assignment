4163b214c14a639570a614245e7c7ff1ee40aeae
Mohammad Faizan <111674496+faizan26696@users.noreply.github.com>
2023-08-15 01:11:20 +0530

    Update ChartGraph.jsx

--- src/Components/ChartGraph/ChartGraph.jsx
+++ src/Components/ChartGraph/ChartGraph.jsx
@@ -3,24 +3,27 @@ import { AreaChart, Area, CartesianGrid, XAxis, YAxis } from "recharts";
 
 const ChartGraph = () => {
   const data = [
-    { name: "23%", students: 23 },
-    { name: "29%", students: 29 },
-    { name: "58%", students: 58 },
-    { name: "75%", students: 75 },
-    { name: "33%", students: 33 },
-    { name: "20%", students: 20 },
-    { name: "73%", students: 73 },
-    { name: "49%", students: 49 },
+    { percent: "23%", temp: 23 },
+    { percent: "29%", temp: 29 },
+    { percent: "58%", temp: 58 },
+    { percent: "75%", temp: 75 },
+    { percent: "33%", temp: 33 },
+    { percent: "20%", temp: 20 },
+    { percent: "73%", temp: 73 },
+    { percent: "49%", temp: 49 },
   ];
 
   return (
     <AreaChart width={812} height={90} data={data}>
-      <Area dataKey="students" fill="blue" stroke="white" />
+      <Area dataKey="temp" fill="blue" stroke="white" />
       <CartesianGrid strokeOpacity={0} />
-      <XAxis dataKey="name" fontSize="14px" tick={false} padding="2rem" />
+      <XAxis dataKey="percent" fontSize="14px" tick={false} padding="2rem" />
       <YAxis axisLine={false} tick={false} />
     </AreaChart>
   );
 };
+
+export default ChartGraph;
+
 
 export default ChartGraph;
